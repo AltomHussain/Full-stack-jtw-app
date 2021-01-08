1ba693e899d2122efc7a237b9bc6ba4bc2aa1e5e
altom <altomhussian2001@yahoo.com>
2021-01-08 21:14:58 +0000

    Added the register inputs

--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -4,7 +4,12 @@ export default function Register() {
   return (
     <Fragment>
       <h1>Resgister</h1>
-     
+      
+      <form>
+        <input type="email" name="email" placeholder="email" className="form-control"/>
+        <input type="password" name="password" placeholder="password" className="form-control"/>
+        <input type="text" name="name" placeholder="name" className="form-control"/>
+      </form>
     </Fragment>
   );
 }
