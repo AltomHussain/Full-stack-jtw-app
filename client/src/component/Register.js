a6961077069138bfab71900aacc70786c452e48e
altom <altomhussian2001@yahoo.com>
2021-01-08 21:37:10 +0000

    added fetch request to the regester form

--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -1,14 +1,64 @@
 import React, { Fragment, useState } from "react";
 
 export default function Register() {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+  const {email, password, name} = inputs;
+  const onChange = (e)=>{
+      setInputs({...inputs, [e.target.name]: e.target.value})
+  }
+const onSubmitForm = async(e)=>{
+    e.preventDefault();
+    const body = {email, password, name}
+    try {
+        const res = await fetch("http://localhost:5000/auth/register",{
+            method: "POST",
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify(body)
+        })
+        const parseRes = await res.json()
+
+        console.log(parseRes);
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
   return (
     <Fragment>
-      <h1>Resgister</h1>
-      
-      <form>
-        <input type="email" name="email" placeholder="email" className="form-control"/>
-        <input type="password" name="password" placeholder="password" className="form-control"/>
-        <input type="text" name="name" placeholder="name" className="form-control"/>
+      <h1 className="text-center my-5">Resgister</h1>
+
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          className="form-control my-3"
+          value={email}
+          onChange={(e)=>onChange(e)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          className="form-control my-3"
+          value={password}
+          onChange={(e)=>onChange(e)}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          className="form-control my-3"
+          value={name}
+          onChange={(e)=>onChange(e)}
+        />
+        <button className="btn btn-success btn-block my-3">Submit</button>
       </form>
     </Fragment>
   );
